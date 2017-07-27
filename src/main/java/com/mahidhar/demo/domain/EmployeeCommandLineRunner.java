a618672a4d10d6b1f94f42064fe9049f66f789cb
Mahidhar <g.m.mahidharreddy@gmail.com>
2017-07-27 08:30:10 +0200

    Sample Data Changed

--- src/main/java/com/mahidhar/demo/domain/EmployeeCommandLineRunner.java
+++ src/main/java/com/mahidhar/demo/domain/EmployeeCommandLineRunner.java
@@ -24,16 +24,16 @@ public class EmployeeCommandLineRunner implements CommandLineRunner {
 		
 		
 		
-		employeeRepository.save(new Employee("Mahidhar","Reddy","mahi@gmail.com","cologne","cologne","IT","Architect",new BigDecimal("55000.00")));
-		employeeRepository.save(new Employee("Rama","Rao","rama@gmail.com","cologne","cologne","IT","Architect",new BigDecimal("55000.00")));
-		employeeRepository.save(new Employee("Raja","Reddy","raja@gmail.com","cologne","cologne","IT","Architect",new BigDecimal("55000.00")));
-		employeeRepository.save(new Employee("Raghu","Nayak","raghu@gmail.com","cologne","cologne","IT","Architect",new BigDecimal("55000.00")));
-		employeeRepository.save(new Employee("Prasad","Naidu","prasad@gmail.com","cologne","cologne","IT","Architect",new BigDecimal("55000.00")));
-		employeeRepository.save(new Employee("Raman","Singh","Raman@gmail.com","cologne","cologne","IT","Architect",new BigDecimal("55000.00")));
-		employeeRepository.save(new Employee("Chintu","Reddy","chintu@gmail.com","cologne","cologne","IT","Architect",new BigDecimal("55000.00")));
-		employeeRepository.save(new Employee("Madhu","Agarwal","madhu@gmail.com","cologne","cologne","IT","Architect",new BigDecimal("55000.00")));
-		employeeRepository.save(new Employee("Sasi","Reddy","sasi@gmail.com","cologne","cologne","IT","Architect",new BigDecimal("55000.00")));
-		employeeRepository.save(new Employee("Veer","Manohar","veer@gmail.com","cologne","cologne","IT","Architect",new BigDecimal("55000.00")));
+		employeeRepository.save(new Employee("Nicolas","Cage","nicolas@gmail.com","Cologne","Cologne","IT","Architect",new BigDecimal("70000.00")));
+		employeeRepository.save(new Employee("Johnny","Depp","johnny@gmail.com","Frankfurt","Frankfurt","Finance","Manager",new BigDecimal("80000.00")));
+		employeeRepository.save(new Employee("Al","pacino","pacino@gmail.com","Los Angeles","Los Angeles","IT","Consultant",new BigDecimal("90000.00")));
+		employeeRepository.save(new Employee("Russel","Crowe","russel@gmail.com","Sydney","Sydney","Marketing","Sales Head",new BigDecimal("120000.00")));
+		employeeRepository.save(new Employee("Denzel","Washington","denzel@gmail.com","New York","New York","HR","Manager",new BigDecimal("80000.00")));
+		employeeRepository.save(new Employee("Brad","Pitt","brad@gmail.com","London","London","IT","SAP Consultant",new BigDecimal("70000.00")));
+		employeeRepository.save(new Employee("Leonardo","Dicaprio","leonardo@gmail.com","Munich","Munich","IT","Principal Consultant",new BigDecimal("120000.00")));
+		employeeRepository.save(new Employee("Sylvester","Stallone","sylvester@gmail.com","Amsterdam","Amsterdam","Finance","Senior Manager",new BigDecimal("90000.00")));
+		employeeRepository.save(new Employee("Keanu","Reeves","keanu@gmail.com","Brussels","Brussels","IT","Software Engineer",new BigDecimal("60000.00")));
+		employeeRepository.save(new Employee("Hugh","Jackman","jackman@gmail.com","Paris","Paris","IT","Software Engineer",new BigDecimal("70000.00")));
 
 		log.info("Initially loaded employees are.....");
 		
